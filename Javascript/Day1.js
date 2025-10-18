e47dd525d0a7d1d0dc8c61026250f8e41a2b399b
Gowtham-7777 <gowtham16jo@gmail.com>
2025-10-18 22:57:31 +0530

    JS

---
+++ Javascript/Day1.js
@@ -0,0 +1,18 @@
+// console.log("Revamp 1");
+// console.log("Revamp 2");
+// console.log("Revamp 3");
+// console.log("Revamp 4");
+// var name="Revamp";
+// var name="5";
+// console.log(name); 
+
+// {
+//     let name="Revamp";
+//     name="5";
+
+// console.log(name);
+// }
+{
+const n="Revamp";
+}
+console.log(n);
